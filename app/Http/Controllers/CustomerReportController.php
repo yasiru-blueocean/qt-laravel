c284bb8102883481475d9a10f152c18e03ef5617
Hamshika <hamshikaraja24@gmail.com>
2025-07-03 10:03:39 +0530

    Update CustomerReportController.php

--- app/Http/Controllers/CustomerReportController.php
+++ app/Http/Controllers/CustomerReportController.php
@@ -16,36 +16,36 @@ class CustomerReportController extends Controller
 
         // Filters
         if ($request->filled('selectedProject') && $request->selectedProject !== 'SelectProject') {
-            $query->whereHas('sales', function($q) use ($request) {
-            $q->where('project_id', $request->selectedProject)
-              ->whereNotIn('active_status', [1,2]);
+            $query->whereHas('sales', function ($q) use ($request) {
+                $q->where('project_id', $request->selectedProject)
+                  ->whereNotIn('active_status', [1, 2]);
             });
         }
         if ($request->filled('selectedCompany') && $request->selectedCompany !== 'SelectCompany') {
-            $query->whereHas('sales', function($q) use ($request) {
-            $q->whereHas('project', function($q2) use ($request) {
-                $q2->where('company_id', $request->selectedCompany);
-            })
-            ->whereNotIn('active_status', [1,2]);
+            $query->whereHas('sales', function ($q) use ($request) {
+                $q->whereHas('project', function ($q2) use ($request) {
+                    $q2->where('company_id', $request->selectedCompany);
+                })
+                ->whereNotIn('active_status', [1, 2]);
             });
         }
         if ($request->filled('selectedSalesMember') && $request->selectedSalesMember !== 'SelectSalesMember') {
-            $query->whereHas('sales', function($q) use ($request) {
-            $q->where('sale_by', $request->selectedSalesMember)
-              ->whereNotIn('active_status', [1,2]);
+            $query->whereHas('sales', function ($q) use ($request) {
+                $q->where('sale_by', $request->selectedSalesMember)
+                  ->whereNotIn('active_status', [1, 2]);
             });
         }
         if ($request->filled('selectedHandlingPerson') && $request->selectedHandlingPerson !== 'SelectHandlingPerson') {
-            $query->whereHas('sales', function($q) use ($request) {
-            $q->where('handling_person', $request->selectedHandlingPerson)
-              ->whereNotIn('active_status', [1,2]);
+            $query->whereHas('sales', function ($q) use ($request) {
+                $q->where('handling_person', $request->selectedHandlingPerson)
+                  ->whereNotIn('active_status', [1, 2]);
             });
         }
 
         $customers = $query->with([
-            'sales' => function($q) {
-            $q->whereNotIn('active_status', [1,2])
-              ->with(['user', 'project.company', 'unit']);
+            'sales' => function ($q) {
+                $q->whereNotIn('active_status', [1, 2])
+                  ->with(['user', 'project.company', 'unit']);
             }
         ])->get();
 
@@ -63,23 +63,21 @@ class CustomerReportController extends Controller
                     $purchases[] = $sale->sale_id;
                     $salesPersons[] = $sale->user ? $sale->user->U_FName : '';
                     $companies[] = $sale->project && $sale->project->company ? $sale->project->company->company_name : '';
-
                     $projects[] = $sale->project ? $sale->project->project_name : '';
-
                     $units[] = $sale->unit ? $sale->unit->unit_Name : '';
                 }
                 $total_purchases += count($purchases);
                 $results[] = [
-                    'C_Title'        => $customer->C_Title,
-                    'C_namewinitials'=> $customer->C_namewinitials,
-                    'C_NIC'          => $customer->C_NIC,
-                    'purchase_ids'   => $purchases,
-                    'f_name_array'   => $salesPersons,
-                    'company_names'  => $companies,
-                    'project_names'  => $projects,
-                    'unit_names'     => $units,
-                    'customer_id'    => $customer->Customer_id,
-                    'purchase_count' => count($purchases),
+                    'C_Title'         => $customer->C_Title,
+                    'C_namewinitials' => $customer->C_namewinitials,
+                    'C_NIC'           => $customer->C_NIC,
+                    'purchase_ids'    => $purchases,
+                    'f_name_array'    => $salesPersons,
+                    'company_names'   => $companies,
+                    'project_names'   => $projects,
+                    'unit_names'      => $units,
+                    'customer_id'     => $customer->Customer_id,
+                    'purchase_count'  => count($purchases),
                 ];
             }
         }
@@ -107,14 +105,11 @@ class CustomerReportController extends Controller
             $filterDetails['handling_person_name'] = $user ? $user->U_FName . ' ' . $user->U_LName : null;
         }
 
-
-       return response()->json([
+        return response()->json([
             'total_customers' => $total_customers,
             'total_purchases' => $total_purchases,
             'customers'       => $results,
             'filters'         => $filterDetails
         ]);
-
     }
 }
-
